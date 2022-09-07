25e78ae6f731905a72550faa8b7ed79fb729d3ad
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-09-07 14:52:08 -0300

    UseEffect functions are added for API consumption and the initial state of the gitHubUsers variable is set

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function MyApi() {
-  return (
-    <div>
-      
-    </div>
-  )
+	const [gitHubUsers, setGitHubUsers] = useState([]);
+
+	useEffect(() => {
+		fetchGitHubUsers();
+	}, []);
+
+	const fetchGitHubUsers = async () => {
+		const url = "https://api.github.com/users";
+		const response = await fetch(url, {
+			method: "GET",
+			headers: new Headers({ Accept: "application/vnd.github+json" }),
+		});
+		const data = await response.json();
+		console.log(data);
+	};
+
+	return <></>;
 }
 
-export default MyApi
+export default MyApi;
