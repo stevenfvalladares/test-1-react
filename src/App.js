98d3cdc2ede590cfb8c98acbf82b0c76f8c62260
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-09-07 14:50:09 -0300

    The HTML structure of the landing page in the main App.js file is modified and the size of the logo is modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
+
 import "./App.css";
 import logo from "./logo.png";
+
+import MyApi from "./components/MyApi";
 
 /* import of the main styles of Bootstrap and its components */
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,24 +11,23 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
 	return (
-		<>
-			<header className="py-3 mb-4 border-bottom">
-				<div className="container d-flex flex-wrap justify-content-center">
-					<div className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-						<img src={logo} className="logo me-2" alt="logo" />
-						<span className="fs-3 text-success">Farmacias de turno</span>
-					</div>
-					<form className="col-12 col-lg-3 d-flex align-items-center mb-3 mb-lg-0">
+		<div className="container-sm">
+			<header className="py-3 mb-4">
+				<div className="d-flex flex-column justify-content-center align-items-center">
+					<img src={logo} className="logo me-2 mb-2" alt="logo" />
+					<span className="fs-3 mb-2">Usuarios de GitHub</span>
+					<form className="col-12 col-lg-4 d-flex align-items-center mb-2 mb-lg-0">
 						<input
 							type="search"
 							className="form-control"
-							placeholder="Buscar por comuna"
+							placeholder="Buscar..."
 							aria-label="Search"
 						/>
 					</form>
 				</div>
 			</header>
-		</>
+			<MyApi />
+		</div>
 	);
 }
 
