36d1d42909c9579686840f7b3b7041d28bd6d2cc
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-09-07 15:28:28 -0300

    Required material UI components are imported into the myApi.jsx file to display a list

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+/* import of the main styles of material UI and its components */
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
 function MyApi() {
 	const [gitHubUsers, setGitHubUsers] = useState([]);
 
@@ -17,7 +26,36 @@ function MyApi() {
 		console.log(data);
 	};
 
-	return <></>;
+	return (
+		<div className="container-sm">
+			<div className="d-flex flex-column justify-content-center align-items-center">
+				<List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+					<ListItem alignItems="flex-start">
+						<ListItemAvatar>
+							<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+						</ListItemAvatar>
+						<ListItemText
+							primary="Brunch this weekend?"
+							secondary={
+								<React.Fragment>
+									<Typography
+										sx={{ display: "inline" }}
+										component="span"
+										variant="body2"
+										color="text.primary"
+									>
+										Ali Connors
+									</Typography>
+									{" — I'll be in your neighborhood doing errands this…"}
+								</React.Fragment>
+							}
+						/>
+					</ListItem>
+					<Divider variant="inset" component="li" />
+				</List>
+			</div>
+		</div>
+	);
 }
 
 export default MyApi;
